4c07e5836b4772d468cc3c743a0da5bbcf00d23f
Fredrik <fredrik.baberg@gmail.com>
2019-06-10 13:09:57 +0000

    Error handling on connection issues.

--- homeassistant/components/octoprint/octoprint.py
+++ homeassistant/components/octoprint/octoprint.py
@@ -1,15 +1,16 @@
 import requests
-import asyncio
+#import asyncio
 import time
 
-import stomper
-import websockets
+#import stomper
+#import websockets
 
 class OctoPrint():
     def __init__(self, host, port):
         self.base_url = 'http://{}:{}'.format(host, port)
         self.headers = {}
         self.api_key = ''
+        self._connected = False
         
     def _set_api_key(self, key):
         """ Set API key and header internally. """
@@ -20,11 +21,15 @@ class OctoPrint():
 
     def get_api_key(self, app_name, user_name=None):
         """ Retrieve API key, stored locally. """
-        response = requests.get(
-            self.base_url+'/plugin/appkeys/probe',
-            headers=self.headers,
-            timeout=9
-        )
+        try:
+            response = requests.get(
+                self.base_url+'/plugin/appkeys/probe',
+                headers=self.headers,
+                timeout=9
+            )
+        except requests.exceptions.ConnectionError:
+            print("Could not connect to server, Connection Refused.")
+            return False
         # If response status code is 204, can continue.
         if response.status_code == 204:
             response = requests.post(
@@ -56,153 +61,199 @@ class OctoPrint():
                     time.sleep(1.0)
                     if response.status_code == 200:
                         self._set_api_key(response.json()['api_key'])
+                        self._connected = True
                         return True
         return False
 
-    def retrieve_appkeys(self):
-        response = requests.get(
-            self.base_url+'/api/plugin/appkeys',
-            headers=self.headers,
-            timeout=9
-        )
-        return response.json()
-        
     def deregister(self):
         """ Remove API key from OctoPrint instance """
-        response = requests.post(
-            url=self.base_url+'/api/plugin/appkeys',
-            headers=self.headers,
-            json={
-                "command": "revoke",
-                "key": self.api_key
-            },
-            timeout=9
-        )
-        if response.status_code == 204:
-            # Successfully removed API key.
-            return True
+        try:
+            response = requests.post(
+                url=self.base_url+'/api/plugin/appkeys',
+                headers=self.headers,
+                json={
+                    "command": "revoke",
+                    "key": self.api_key
+                },
+                timeout=9
+            )
+            if response.status_code == 204:
+                # Successfully removed API key.
+                self._connected = False
+                return True
+        except requests.exceptions.ConnectionError:
+            print("Could not connect.")
         return False
+    
+    def retrieve_appkeys(self):
+        try:
+            return requests.get(
+                self.base_url+'/api/plugin/appkeys',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_version(self):
-        return requests.get(
-            url=self.base_url+'/api/version',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/version',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
 
     def get_printer_status(self):
         """ Retrieve printer status """
-        response = requests.get(
-            url=self.base_url+'/api/printer',
-            headers=self.headers,
-            timeout=9
-        )
-        return response.json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printer',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_connection(self):
         """ Retrieve printer connections """
-        response = requests.get(
-            url=self.base_url+'/api/connection',
-            headers=self.headers,
-            timeout=9
-        )
-        return response.json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/connection',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_files(self):
         """ Retrieve printer files """
-        response = requests.get(
-            url=self.base_url+'/api/files?recursive=true',
-            headers=self.headers,
-            timeout=9
-        )
-        return response.json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/files?recursive=true',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_job(self):
         """ Retrieve printer job information """
-        response = requests.get(
-            url=self.base_url+'/api/job',
-            headers=self.headers,
-            timeout=9
-        )
-        return response.json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/job',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def pause_print(self):
         """ Pause print """
-        response = requests.post(
-            url=self.base_url+'/api/job',
-            headers=self.headers,
-            json={
-                'command': 'pause',
-                'action': 'pause'
-            },
-            timeout=9
-        )
-        return response
+        if self._connected:
+            try:
+                return requests.post(
+                    url=self.base_url+'/api/job',
+                    headers=self.headers,
+                    json={
+                        'command': 'pause',
+                        'action': 'pause'
+                    },
+                    timeout=9
+                )
+            except requests.exceptions.ConnectionError:
+                print("Connection refused.")
+                return False
+        return False
 
     def resume_print(self):
         """ Resume print """
-        response = requests.post(
-            url=self.base_url+'/api/job',
-            headers=self.headers,
-            json={
-                'command': 'pause',
-                'action': 'resume'
-            },
-            timeout=9
-        )
-        return response
+        if self._connected:
+            try:
+                return requests.post(
+                    url=self.base_url+'/api/job',
+                    headers=self.headers,
+                    json={
+                        'command': 'pause',
+                        'action': 'resume'
+                    },
+                    timeout=9
+                )
+            except requests.exceptions.ConnectionError:
+                    print("Connection refused.")
+                    return False
+        return False
         
     def get_printer_tool_state(self):
         """ Get printer tool state """
-        return requests.get(
-            url=self.base_url+'/api/printer/tool',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printer/tool',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_bed_state(self):
         """ Get printer tool state """
-        return requests.get(
-            url=self.base_url+'/api/printer/bed',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printer/bed',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_chamber_state(self):
         """ Get printer tool state """
-        return requests.get(
-            url=self.base_url+'/api/printer/chamber',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printer/chamber',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_sd_state(self):
         """ Get printer tool state """
-        return requests.get(
-            url=self.base_url+'/api/printer/sd',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printer/sd',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_profiles(self):
         """ Get printer profiles """
-        return requests.get(
-            url=self.base_url+'/api/printerprofiles',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/printerprofiles',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_printer_settings(self):
         """ Get printer profiles """
-        return requests.get(
-            url=self.base_url+'/api/settings',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/settings',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def set_printer_settings(self):
         """ Set settings for OctoPrint """
-        response = requests.post(
+        return requests.post(
             url=self.base_url+'/api/settings',
             headers=self.headers,
             json={
@@ -212,40 +263,49 @@ class OctoPrint():
             },
             timeout=9
         )
-        return response
         
     def get_slicing(self):
         """ Get slicers and slicing profiles """
-        return requests.get(
-            url=self.base_url+'/api/slicing',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/slicing',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_system_commands(self):
         """ Get slicers and slicing profiles """
-        return requests.get(
-            url=self.base_url+'/api/system/commands',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/system/commands',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
         
     def get_timelapse(self):
         """ Retrieve a list of timelapses and the current config """
-        return requests.get(
-            url=self.base_url+'/api/timelapse',
-            headers=self.headers,
-            timeout=9
-        ).json()
+        try:
+            return requests.get(
+                url=self.base_url+'/api/timelapse',
+                headers=self.headers,
+                timeout=9
+            ).json()
+        except ValueError:
+            return False
 
 
 if __name__=='__main__':
     OP = OctoPrint("127.0.0.1", 5000)
-    #print(OP.get_api_key("Home-Assistant"))
-    OP._set_api_key('abc')
+    print(OP.get_api_key("Home-Assistant"))
+    #OP._set_api_key('abc')
     print(OP.retrieve_appkeys())
     print(OP.get_printer_status())
     print(OP.get_printer_version())
+    print(OP.get_printer_status())
     print(OP.get_printer_connection())
     print(OP.get_printer_files())
     print(OP.get_printer_job())
@@ -255,10 +315,11 @@ if __name__=='__main__':
     print(OP.get_printer_bed_state())
     print(OP.get_printer_chamber_state())
     print(OP.get_printer_sd_state())
+    print(OP.get_printer_sd_state())
     print(OP.get_printer_profiles())
     print(OP.get_printer_settings())
     print(OP.set_printer_settings())
     print(OP.get_slicing())
     print(OP.get_system_commands())
     print(OP.get_timelapse())
-    #print(OP.deregister())
+    print(OP.deregister())
